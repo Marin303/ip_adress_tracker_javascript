592cfc1e98c97f69ae44afa10addc223045a0149
Marin <marin.muktic3@gmail.com>
2023-06-20 20:53:05 +0200

    map included, media query fix,better responsive view

--- script.js
+++ script.js
@@ -34,3 +34,16 @@ function provideInfo(data) {
   timezoneInfo.innerHTML = `<span>TIMEZONE</span> <p>${data.location.timezone}</p>`;
   ipsInfo.innerHTML = `<span>ISP</span><p>${data.isp}</p>`;
 }
+let map = L.map("map").setView([51.505, -0.09], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+let marker = L.marker([51.5, -0.09]).addTo(map);
+let circle = L.circle([51.508, -0.11], {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 0.5,
+  radius: 500,
+}).addTo(map);
